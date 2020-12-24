d40b29ea07f6a2b209e0d15c1f99ba24d84ffc43
superbrace <rongcanyo@gmail.com>
2020-12-25 00:39:28 +0800

    defalut parameter try 1

--- CS106/Lecture2-Function/main.cpp
+++ CS106/Lecture2-Function/main.cpp
@@ -25,5 +25,10 @@ int main() {
     drawLine();
     drawLine(10);
     drawLine(7,'?');
+    drawLine('x');
+    cout << (int) 'x';
+    //(int)'x' == 120
+    // defalut parameter is base on position
+    //************************************************************************************************************************
     return 0;
 }
