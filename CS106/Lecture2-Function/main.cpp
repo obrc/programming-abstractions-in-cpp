71b715dd6f6cf730e2b854dcb82a37a9acc126e8
superbrace <rongcanyo@gmail.com>
2020-12-25 10:07:13 +0800

    output parameter and quadratic excercise

--- CS106/Lecture2-Function/main.cpp
+++ CS106/Lecture2-Function/main.cpp
@@ -12,18 +12,21 @@
  */
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-void drawLine(int width, char c = '*') {
-    for (int i = 0; i < width; i++) {
-        cout << c;
-    }
-    cout << endl;
+void quadratic(int a, int b, int c, double &root1, double &root2) {
+    // a, b, c just use the vlaue, so set as values parameter
+    // root1, root2, should carry the result to main function, so set as reference parameter
+    double d = b * b - 4 * a * c;
+    d = sqrt(d);
+    root1 = (-b + d) / (2 * a);
+    root2 = (-b - d) / (2 * a);
 }
 
 int main() {
-    //drawLine(); //no match
-    drawLine(10);
-    drawLine(7,'?');
+    double root1, root2;
+    quadratic(1,-3,-4,root1,root2);
+    cout << "root1 = " << root1 << ", root2 = " << root2 << endl;
     return 0;
 }
