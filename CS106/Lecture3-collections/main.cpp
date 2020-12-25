fb6f0fc7e213cad26d9abbfafed36bc892a29d9d
superbrace <rongcanyo@gmail.com>
2020-12-25 22:41:43 +0800

    vector size and access with [index]

--- CS106/Lecture3-collections/main.cpp
+++ CS106/Lecture3-collections/main.cpp
@@ -4,20 +4,21 @@
  * vector
  *  vector is collection elements with 0-base index, aka list
  *  create vector with {}, or add after
+ *  can access with [index]
+ *  size
+ *
  * grid
  */
 
 #include <iostream>
+#include "console.h"
 #include "vector.h"
+
 using namespace std;
 
 int main() {
-    Vector<int> nums = {1,2,3,4,5,89,10};
-    cout << nums << endl;
-    //{1, 2, 3, 4, 5, 89, 10}
-    nums.add(7);
-    nums.add(8);
-    cout << nums << endl;
-    //{1, 2, 3, 4, 5, 89, 10, 7, 8}
+    Vector<int> nums = {42, 23, 15, 899, 1};
+    for (int i = 0; i < nums.size(); i++)
+        cout << nums[i] << " ";
     return 0;
 }
