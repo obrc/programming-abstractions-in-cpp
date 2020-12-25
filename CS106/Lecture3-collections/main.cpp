14b77837ec6ad1299f250fceb81b4c5194951001
superbrace <rongcanyo@gmail.com>
2020-12-25 22:51:06 +0800

    iterating over vector

--- CS106/Lecture3-collections/main.cpp
+++ CS106/Lecture3-collections/main.cpp
@@ -6,7 +6,10 @@
  *  create vector with {}, or add after
  *  can access with [index]
  *  size
- *
+ *  iterating over
+ *      for loop
+ *      for loop backward
+ *      "for-each" loop
  * grid
  */
 
@@ -17,10 +20,25 @@
 using namespace std;
 
 int main() {
+    Vector<int> seq = {1,2,3,4,5,6,7,8,9,10};
+    // for loop
+    for (int i = 0; i < seq.size(); i++){
+        cout << seq.get(i) << " ";
+    }
+    cout << endl;
+    // for loop backward
+    for (int i = seq.size() - 1; i >= 0; i--) {
+        cout << seq.get(i) << " ";
+    }
+    cout << endl;
     Vector<string> names;
     names.add("zhao qian");
     names.add("sun li");
     names.add("bai jia xing");
-    cout << names << endl;
+    // for-each loop
+    for (string name : names) {
+        cout << name << " ";
+    }
+    cout << endl;
     return 0;
 }
